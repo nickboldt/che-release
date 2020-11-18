1a6ffc1f9b63e7dfec94c1f45f3541d83a0c317f
nickboldt <nboldt@redhat.com>
2020-11-18 16:19:45 -0500

    add instructions for quay and docker login requirements; clean up script; add steps to fix up repos so they're public and have robot/team access
    Change-Id: I5959d2bc80e7537aa1697945552dec63703aacd3
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- utils/copyImagesToQuay.sh
+++ utils/copyImagesToQuay.sh
@@ -29,9 +29,9 @@ checkVersion() {
 }
 checkVersion 0.40 "$(skopeo --version | sed -e "s/skopeo version //")" skopeo
 
-DOCOPY=1    # normally, do the copy; optionally can just list images to copy
-QUIET=0 	# less output - omit container tag URLs
-VERBOSE=1	# more output
+DOCOPY=1      # normally, do the copy; optionally can just list images to copy
+AUTH_TOKEN="" # API token to use to make changes to the repos (if applicable)
+VERBOSE=0	# more output
 WORKDIR=$(pwd)
 
 usage () {
@@ -45,16 +45,23 @@ usage () {
 export QUAY_TOKEN=\"your token goes here\"
 echo \"\${QUAY_TOKEN}\" | podman login -u=\"\${QUAY_USER}\" --password-stdin quay.io
 
-4. Finally, run this script. Note that if your connection to quay.io times out pulling or pushing, 
+4. Repeat the above process for docker.io - you just need a login that won't be rate-limited 
+when reading digest information with skopeo:
+
+export DOCKER_TOKEN=\"your token goes here\"
+echo \"\${DOCKER_TOKEN}\" | podman login -u=\"\${DOCKER_USER}\" --password-stdin docker.io
+
+5. Finally, run this script. Note that if your connection to quay.io times out pulling or pushing, 
 you can re-run this script until it's successful.
 
 Usage:   $0 -f [IMAGE LIST FILE] [--nocopy]
 Example: $0 -f copyImagesToQuay.txt --nocopy
 
 Options: 
-	-q, -v             quiet, verbose output
-	--nocopy           just collect the list of destination images + SHAs, but don't actually do the copy
-	--help, -h         help
+	-v               verbose output
+	--no-copy        just collect the list of destination images + SHAs, but don't actually do the copy
+	--auth-token     if you want to also set user/group/visibility settings on the target repo, provide your auth token
+	--help, -h       help
 "
 	exit 
 }
@@ -65,9 +72,9 @@ while [[ "$#" -gt 0 ]]; do
   case $1 in
     '-w') WORKDIR="$2"; shift 1;;
     '-f') LISTFILE="$2"; shift 1;;
-    '-q') QUIET=1; shift 0;;
-    '-v') QUIET=0; VERBOSE=1; shift 0;;
-	'--nocopy') DOCOPY=0; shift 0;;
+    '-v') VERBOSE=1; shift 0;;
+	'--no-copy') DOCOPY=0; shift 0;;
+	'--auth-token') AUTH_TOKEN="$2"; shift 1;;
     '--help'|'-h') usage;;
     *) OTHER="${OTHER} $1"; shift 0;; 
   esac
@@ -77,6 +84,21 @@ done
 # check for valid list file
 if [[ ! $LISTFILE ]]; then usage; fi
 if [[ ! -r $LISTFILE ]] && [[ ! -r ${WORKDIR}/${LISTFILE} ]]; then usage; fi
+
+curl_it ()
+{
+	# 1 - method = PUT, POST, GET
+	# 2 - api path, see https://docs.quay.io/api/swagger/#!/permission/changeUserPermissions for syntax
+	# 3 - JSON payload, see https://docs.quay.io/api/swagger/#!/permission/changeUserPermissions for examples
+
+	if [[ $VERBOSE -gt 0 ]]; then
+		set -x
+		curl -sSL -X "${1}" -H "Authorization: Bearer $AUTH_TOKEN" -H "Content-Type: application/json" "https://quay.io/api/v1/repository/${imagePath}/${2}" -d "${3}" | jq .
+		set +x
+	else
+		curl -sSL -X "${1}" -H "Authorization: Bearer $AUTH_TOKEN" -H "Content-Type: application/json" "https://quay.io/api/v1/repository/${imagePath}/${2}" -d "${3}" | grep "not allowed" || true
+	fi
+}
 
 while IFS= read -r image; do
 	if [[ ${image} ]]; then 
@@ -88,13 +110,33 @@ while IFS= read -r image; do
 			imageNew="${imageNew}:latest"
 		fi
 		# get the digest of the image and use that instead of a potentially moving tag
-		digest="$(skopeo inspect docker://${image} | yq -r '.Digest' | sed -r -e "s#sha256:##g")"
-		if [[ ${VERBOSE} -eq 1 ]]; then echo "
+		digest=""
+		if [[ ${DOCOPY} -eq 1 ]]; then
+			if [[ $VERBOSE -gt 0 ]]; then set -x; fi
+			digest="$(skopeo inspect docker://${image} | yq -r '.Digest' | sed -r -e "s#sha256:#-#g")"
+			echo "
 [INFO] Skopeo copy $image to
-        ${imageNew}-${digest} ... "
+        ${imageNew}${digest} ... "
+			# note that the target image in quay must exist; or the user pushing must be an administrator to create the repo on the fly
+			skopeo copy --all "docker://${image}" "docker://${imageNew}${digest}"
+			set +x
 		fi
-		# note that the target image in quay must exist; or the user pushing must be an administrator to create the repo on the fly
-		if [[ ${DOCOPY} -eq 1 ]]; then skopeo copy --all "docker://${image}" "docker://${imageNew}-${digest}"; fi
+
+		# now make sure we have valid permissions on the repo: 
+		if [[ ${AUTH_TOKEN} ]]; then
+			imagePath=${imageNew%%:*}
+			imagePath=${imagePath#*/}
+			echo "[INFO] Update https://quay.io/repository/${imagePath}?tab=settings ..."
+			echo "[INFO]  + Make public ..."
+			curl_it POST changevisibility '{ "visibility": "public" }'
+			echo "[INFO]  + Add team owner = admin ..."
+			curl_it PUT permissions/team/owners '{ "role": "admin" }'
+			echo "[INFO]  + Add team creators = write ..."
+			curl_it PUT permissions/team/creators '{ "role": "write" }'
+			echo "[INFO]  + Add user robots (2) = write ..."
+			curl_it PUT permissions/user/eclipse+centos_ci '{ "role": "write" }'
+			curl_it PUT permissions/user/eclipse+gh_actions_dockerfiles '{ "role": "write" }'
+		fi
 		digest=""
 	fi
 done < <(grep -v '^ *#' < ${LISTFILE}) # exclude commented lines
